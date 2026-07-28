860a359fae0fec3335c87667443fed46a02984f6
Rui Carmo <rui.carmo@gmail.com>
2026-07-28 21:55:13 +0100

    tools: add compiler SPIR-V include path

--- tools/vulkan-toolchain-env.sh
+++ tools/vulkan-toolchain-env.sh
@@ -5,5 +5,7 @@ export PATH="$_vulkan_prefix/bin:$PATH"
 export LD_LIBRARY_PATH="$_vulkan_prefix/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH:-}"
 export CMAKE_PREFIX_PATH="$_vulkan_prefix:${CMAKE_PREFIX_PATH:-}"
 export CMAKE_INCLUDE_PATH="$_vulkan_prefix/include:${CMAKE_INCLUDE_PATH:-}"
+export CPLUS_INCLUDE_PATH="$_vulkan_prefix/include:${CPLUS_INCLUDE_PATH:-}"
+export C_INCLUDE_PATH="$_vulkan_prefix/include:${C_INCLUDE_PATH:-}"
 export Vulkan_GLSLC_EXECUTABLE="$_vulkan_prefix/bin/glslc"
 unset _vulkan_prefix
