9aa96a8178e7bb1b839ad0cdfe31b3c3815c1bab
Rui Carmo <rui.carmo@gmail.com>
2026-07-28 21:53:39 +0100

    tools: expose local SPIR-V headers to Vulkan builds

--- tools/vulkan-toolchain-env.sh
+++ tools/vulkan-toolchain-env.sh
@@ -4,5 +4,6 @@ _vulkan_prefix=${VULKAN_LOCAL_PREFIX:-/workspace/.local/vulkan-toolchain/usr}
 export PATH="$_vulkan_prefix/bin:$PATH"
 export LD_LIBRARY_PATH="$_vulkan_prefix/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH:-}"
 export CMAKE_PREFIX_PATH="$_vulkan_prefix:${CMAKE_PREFIX_PATH:-}"
+export CMAKE_INCLUDE_PATH="$_vulkan_prefix/include:${CMAKE_INCLUDE_PATH:-}"
 export Vulkan_GLSLC_EXECUTABLE="$_vulkan_prefix/bin/glslc"
 unset _vulkan_prefix
